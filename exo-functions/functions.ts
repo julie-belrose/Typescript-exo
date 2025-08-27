b2b5dd3c29dd85455591b6953e7cb0f83cb206b0
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-08-27 10:28:19 +0200

    feat(exo-functions): complete exo 4 to 5

--- exo-functions/functions.ts
+++ exo-functions/functions.ts
@@ -61,4 +61,79 @@ console.log("Total avec pourboire : " + ajouterPourboire(50, 5));
 console.log("Format contact : " + formatContact());
 console.log("Format contact : " + formatContact({ nom: "Alice", ville: "Lyon" }));
 
+// Exo 3
+const arrondirAuCentime = (n: number): number => {
+    return Math.round(n * 100) / 100;
+};
 
+const appliquerReduction = (prix: number): number => {
+    const REDUCTION = 0.1;
+    return prix * (1 - REDUCTION);
+};
+
+const upperProduits = (liste: string[]): string[] => {
+    return liste.map(product => product.toUpperCase());
+};
+
+const creerContact = (nom: string): { id: string; nom: string } => {
+    return { id: Date.now().toString(), nom };
+};
+
+console.log('Exo 3---');
+console.log("Arrondir au centime : " + arrondirAuCentime(10.123));
+
+console.log("Appliquer réduction : " + appliquerReduction(65));
+console.log("Upper produits : " + upperProduits(["pain", "beurre", "jambon"]));
+
+console.log("Creer contact : " + JSON.stringify(creerContact("Alice")));
+
+
+// Exo 4
+
+const podometre = {
+    pas: 0,
+    increment(): void { this.pas++; },
+    reset(): void { this.pas = 0; },
+    incrementArrow: (() => { // `this` est lexical (hérité du scope extérieur), donc pas accessible (La fonction fléchée est écrite dans le corps de l’objet, donc ce corps d’objet n’est pas un “scope de this)
+        let pasPrivés = 0;        // variable save dans la closure
+        return (): number => ++pasPrivés; // clé qui permet d'y accéder (closure)
+    })()
+};
+
+console.log('Exo 4---');
+podometre.increment();
+console.log("Pas : " + podometre.pas);
+podometre.reset();
+console.log("Pas : " + podometre.pas);
+console.log("Pas avec arrow : " + podometre.incrementArrow());
+
+
+// Exo 5
+
+const logAction = (message: string): void => {
+    console.log((message) ? message : "Action non spécifiée");
+};
+
+const erreurFatale = (message: string): never => {
+    throw new Error(`Erreur fatale : ${message}`);
+};
+
+const testError = (message?: string) => {
+
+    message = `data corrompue`;
+    try {
+        erreurFatale(message);
+    } catch (e) {
+        console.warn("Récupération locale error fatale, on continue :", e);
+    }
+}
+
+const getMeteo = async (ville: string): Promise<{ ville: string; degres: number }> => {
+    return { ville, degres: 21 };
+};
+
+console.log('Exo 5---');
+
+logAction("simple log");
+console.log("testError : " + testError());
+getMeteo("Lyon").then(meteo => console.log(meteo));
